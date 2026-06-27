cc25dc351da70c8d23aa439e32ef796136f61d41
mahirXsharma <mahirsharma2002@gmail.com>
2026-06-27 14:37:36 +0530

    Time: 7 ms (91.4%), Space: 46.4 MB (87.85%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,29 +1,34 @@
 class Solution {
-    class Info{
-        char ch;
-        int freq;
-        public Info(char ch, int freq){
-            this.ch = ch;
-            this.freq = freq;
-        }
-    }
     public String frequencySort(String s) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        PriorityQueue<Info> pq = new PriorityQueue<>((a,b)->b.freq - a.freq);
+        List<Character> bucket[] = new ArrayList[s.length() + 1];
+        int arr[] = new int[128];
+        // store every char
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            arr[curr]++;
+        }
+        // put in the bucket
+        for (int i = 0; i < 128; i++) {
+            int freq = arr[i];
+            if (freq > 0) {
+                if (bucket[freq] == null)
+                    bucket[freq] = new ArrayList<>();
+                bucket[freq].add((char) i);
+            }
+        }
         StringBuilder sb = new StringBuilder("");
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-        for(char key : map.keySet()){
-            pq.add(new Info(key, map.get(key)));
-        }
-        while(!pq.isEmpty()){
-            Info curr = pq.remove();
-            for(int i=0; i<curr.freq; i++) sb.append(curr.ch);
+        // looping backwards in the bucket
+        for (int i = bucket.length - 1; i > 0; i--) {
+            List<Character> curr = bucket[i];
+            if (curr != null) {
+                for (char ch : curr) {
+                    for (int j = 0; j < i; j++) {
+                        sb.append(ch);
+                    }
+                }
+            }
+
         }
         return sb.toString();
     }
-
-
 }
