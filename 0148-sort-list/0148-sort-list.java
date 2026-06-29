c59b7f78146a02d26580f3502733c144e3e0650d
mahirXsharma <mahirsharma2002@gmail.com>
2026-06-29 19:53:20 +0530

    Time: 12 ms (29.86%), Space: 62.1 MB (23.06%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -11,20 +11,70 @@
 class Solution {
     public ListNode sortList(ListNode head) {
         if(head == null || head.next == null) return head;
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)-> a.val-b.val);
         ListNode temp = head;
-        while(temp != null){
-            pq.add(temp);
+        while (temp.next != null) {
             temp = temp.next;
         }
-        ListNode newHead = pq.remove(); 
-        temp = newHead;
-        while(!pq.isEmpty()){
-           ListNode curr = pq.remove();
-            temp.next = curr;
-            temp = curr;
-        }
-        temp.next = null;
+        ListNode newHead = mergeSort(head, temp);
         return newHead;
+    }
+
+    public ListNode mergeSort(ListNode head,ListNode tail){
+        if(head == tail) return head;
+        ListNode mid = midFinder(head, tail);
+        // we have to save the mid.next;
+        ListNode save = mid.next;
+        mid.next = null;
+        ListNode leftHead = mergeSort(head, mid);
+        ListNode rightHead = mergeSort(save, tail);
+        // finding tail of left
+        return merge(leftHead, rightHead);
+    }
+
+    public ListNode merge(ListNode lh, ListNode rh){
+        ListNode head;
+        if(lh.val > rh.val){
+            head = rh;
+            rh = rh.next;
+        } 
+        else {
+            head = lh;
+            lh = lh.next;
+        }
+        ListNode temp = head;
+        while(lh != null && rh != null){
+            if(lh.val > rh.val){
+                temp.next = rh;
+                temp = rh;
+                rh = rh.next;
+            }
+            else {
+                temp.next = lh;
+                temp = lh;
+                lh = lh.next;
+            }
+        }
+        while(lh != null){
+            temp.next = lh;
+            temp = lh;
+            lh = lh.next;
+        }
+        while(rh != null){
+            temp.next = rh;
+            temp = rh;
+            rh = rh.next;
+        }
+        return head;
+    }
+
+    public ListNode midFinder(ListNode head, ListNode tail){
+        
+        ListNode slow =  head;
+        ListNode fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
     }
 }
