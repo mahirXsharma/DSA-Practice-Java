8e798a11a6ab27d46600ba81cebaa93413fe227e
mahirXsharma <mahirsharma2002@gmail.com>
2026-07-05 15:28:02 +0530

    Time: 2 ms (43.4%), Space: 47.3 MB (43.16%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,27 +1,20 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        int ans = 0;
         boolean visited[] = new boolean[n];
         Queue<Integer> q = new ArrayDeque<>();
-        int ans = 0;
-        for(int i=0; i<n; i++) list.add(new ArrayList<>());
-        for(int i=0; i<n; i++){
-            int[] curr = isConnected[i];
-            for(int j=0; j<n; j++){
-                if(isConnected[i][j] == 1) list.get(i).add(j);
-            }
-        }
-        for(int i=0; i<n; i++){
-            if(!visited[i]){
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
                 visited[i] = true;
                 q.add(i);
-                while(!q.isEmpty()){
+                while (!q.isEmpty()) {
                     int curr = q.poll();
-                    for(int bro : list.get(curr)){
-                        if(!visited[bro]){
-                            visited[bro] = true;
-                            q.add(bro);
+                    for (int j = 0; j < n; j++) {
+                        if (isConnected[curr][j] == 1 && !visited[j]) {
+                            visited[j] = true;
+                            q.add(j);
                         }
                     }
                 }
