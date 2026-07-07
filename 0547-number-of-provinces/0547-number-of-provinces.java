0d52b2dc4086b4462cdee635e93c847af65798b3
mahirXsharma <mahirsharma2002@gmail.com>
2026-07-07 11:32:02 +0530

    Time: 1 ms (87.18%), Space: 47.4 MB (43.34%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -9,7 +9,7 @@ class Solution {
         }
 
         for (int i = 0; i < n; i++) {
-            for(int j=0; j<n; j++){
+            for(int j=i+1; j<n; j++){
                 int curr = isConnected[i][j];
                 if(curr == 1 && i != j){
                     boolean didMerge = union(i, j, parent);
