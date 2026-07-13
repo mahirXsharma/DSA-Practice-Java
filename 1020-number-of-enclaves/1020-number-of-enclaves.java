eed14237396f3b7fb1fbdf50a07e9932e11475e1
mahirXsharma <mahirsharma2002@gmail.com>
2026-07-13 16:39:10 +0530

    Time: 9 ms (51.22%), Space: 53.5 MB (87.3%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -4,20 +4,24 @@ class Solution {
         int dirs[][] = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
         Queue<int[]> q = new ArrayDeque<>();
         for (int i = 0; i < m; i++) {
-            if (grid[i][0] == 1)
+            if (grid[i][0] == 1) {
                 q.add(new int[] { i, 0 });
-            grid[i][0] = 2;
-            if (grid[i][n - 1] == 1)
+                grid[i][0] = 2;
+            }
+            if (grid[i][n - 1] == 1) {
                 q.add(new int[] { i, n - 1 });
-            grid[i][n - 1] = 2;
+                grid[i][n - 1] = 2;
+            }
         }
         for (int j = 0; j < n; j++) {
-            if (grid[0][j] == 1)
+            if (grid[0][j] == 1) {
                 q.add(new int[] { 0, j });
-            grid[0][j] = 2;
-            if (grid[m - 1][j] == 1)
+                grid[0][j] = 2;
+            }
+            if (grid[m - 1][j] == 1) {
                 q.add(new int[] { m - 1, j });
-            grid[m - 1][j] = 2;
+                grid[m - 1][j] = 2;
+            }
         }
         while (!q.isEmpty()) {
             int curr[] = q.poll();
