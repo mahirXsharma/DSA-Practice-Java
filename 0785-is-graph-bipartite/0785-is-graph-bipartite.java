9d7febe71baa6d3657ee3a139e98cb989f8744a1
mahirXsharma <mahirsharma2002@gmail.com>
2026-07-14 20:36:38 +0530

    Time: 0 ms (100%), Space: 46.9 MB (89.98%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -2,25 +2,24 @@ class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
         int arr[] = new int[n];
-        Queue<Integer> q = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
             if (arr[i] == 0) {
-                q.add(i);
                 arr[i] = -1;
-                while (!q.isEmpty()) {
-                    int curr = q.poll();
-                    int[] bros = graph[curr];
-                    for (int bro : bros) {
-                        if(arr[bro] == arr[curr]) return false;
-                        if(arr[bro]==0) {
-                            arr[bro] = -arr[curr];
-                            q.add(bro);
-                        }
-                    }
-                }
+                if (dfs(graph, arr, i) == false)
+                    return false;
             }
         }
+        return true;
+    }
 
+    public boolean dfs(int graph[][], int arr[], int curr) {
+        for(int bro : graph[curr]){
+            if(arr[bro] == 0){
+                arr[bro] = -arr[curr];
+                if( dfs(graph, arr, bro) == false) return false;
+            }
+            else if(arr[bro] == arr[curr]) return false;
+        }
         return true;
     }
 }
