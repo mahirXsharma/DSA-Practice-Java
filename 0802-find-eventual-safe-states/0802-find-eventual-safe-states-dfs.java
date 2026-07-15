116fb90a4d8bbaef32aa691beb39ce4b479b1771
mahirXsharma <mahirsharma2002@gmail.com>
2026-07-15 18:30:15 +0530

    Time: 5 ms (68.97%), Space: 63 MB (45.03%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states-dfs.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n = graph.length;
+        boolean pathVis[] =  new boolean[n]; 
+        boolean redRope[] = new boolean[n];
+        List<Integer> ans = new LinkedList<>();
+        for(int i=0; i<n; i++){
+            if(!pathVis[i]) dfs(graph, pathVis, redRope, i);
+        }
+        for(int i=0; i<n; i++){
+            if(!redRope[i]) ans.add(i);
+        }
+        return ans;
+    }
+    public boolean dfs(int graph[][], boolean pathVis[], boolean redRope[], int i){
+        if(redRope[i]) return false;
+        if(pathVis[i]) return true;
+        pathVis[i] = true;
+        redRope[i] = true;
+        
+        for(int bro : graph[i]){
+            if(!dfs(graph, pathVis, redRope, bro)) return false;
+        }
+        redRope[i] = false;
+        return true;
+    }
+}
