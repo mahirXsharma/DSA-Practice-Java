5c434d82b06455b6a43bba425f019b787adeb36b
mahirXsharma <mahirsharma2002@gmail.com>
2026-07-16 17:40:49 +0530

    Time: 16 ms (55.29%), Space: 60.4 MB (85.25%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,27 +1,30 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int n = graph.length;
-        boolean pathVis[] =  new boolean[n]; 
-        boolean redRope[] = new boolean[n];
         List<Integer> ans = new ArrayList<>();
+        Queue<Integer> q = new ArrayDeque<>();
+        int degree[] = new int[n];
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+        for(int i=0; i<n; i++) arr.add(new ArrayList<>());
         for(int i=0; i<n; i++){
-            if(!pathVis[i]) dfs(graph, pathVis, redRope, i);
+            degree[i] = graph[i].length;
+            for(int num : graph[i]){
+                arr.get(num).add(i);
+            }
         }
         for(int i=0; i<n; i++){
-            if(!redRope[i]) ans.add(i);
+            if(degree[i] == 0) q.add(i);
+        }
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            for(int num : arr.get(curr)){
+                degree[num]--;
+                if(degree[num] == 0) q.add(num);
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(degree[i] == 0) ans.add(i);
         }
         return ans;
-    }
-    public boolean dfs(int graph[][], boolean pathVis[], boolean redRope[], int i){
-        if(redRope[i]) return false;
-        if(pathVis[i]) return true;
-        pathVis[i] = true;
-        redRope[i] = true;
-        
-        for(int bro : graph[i]){
-            if(!dfs(graph, pathVis, redRope, bro)) return false;
-        }
-        redRope[i] = false;
-        return true;
     }
 }
