b54375cdf5404af4ed834eb8a9189abfb6fcb864
mahirXsharma <mahirsharma2002@gmail.com>
2026-07-16 14:31:38 +0530

    Time: 5 ms (83.24%), Space: 46.8 MB (53.43%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,43 +1,31 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int p[] = new int[1];
-        int n = numCourses;
+        int n = numCourses, p = n-1;
         ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+        Queue<Integer> q = new ArrayDeque<>();
+        int degree[] = new int[n];
+        int ans[] = new int[n];
         for (int i = 0; i < n; i++)
             arr.add(new ArrayList<>());
         for (int[] curr : prerequisites) {
             arr.get(curr[0]).add(curr[1]);
+            degree[curr[1]] ++;
         }
-        boolean pathVis[] = new boolean[n];
-        boolean redRope[] = new boolean[n];
-        int ans[] = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            if(!pathVis[i]){ 
-                if(!dfs(arr, pathVis, redRope, ans, i, p) ){
-                    return new int[]{};
-                }
+        for(int i=0; i<n; i++){
+            if(degree[i]== 0) q.add(i);
+        }
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            ans[p--] = curr;
+            for(int num : arr.get(curr)){
+                degree[num]--;
+                if(degree[num] == 0)q.add(num);
             }
         }
-        return ans;
-    }
-
-    public boolean dfs(ArrayList<ArrayList<Integer>> arr, boolean[] pathVis, boolean[] redRope, int[] ans, int i, int[] p) {
-        // i think i can remove this first condition
-        if(redRope[i]) return false;
-        if(pathVis[i]) return true;
-        pathVis[i] = true;
-        redRope[i] = true;
-        if(!arr.get(i).isEmpty()){
-            for(int bro : arr.get(i)){
-                if(!dfs(arr, pathVis, redRope, ans, bro, p)) return false;
-
-            }   
+        for(int num : degree){
+            if(num != 0) return new int[]{};
         }
-        redRope[i] = false;
-        ans[p[0]++] = i;
-        return true;
-
+        return ans;
     }
 }
 
