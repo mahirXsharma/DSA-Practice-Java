3377bc196e01e74aabdfe80897a37779b6b3854c
mahirXsharma <mahirsharma2002@gmail.com>
2026-07-16 14:09:07 +0530

    Time: 6 ms (72.19%), Space: 47.2 MB (49.49%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,36 +1,31 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        boolean vis[] = new boolean[numCourses];
-        boolean pathVis[] = new boolean[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            list.add(new ArrayList<>());
-        }
-        for (int[] curr : prerequisites) {
-            list.get(curr[1]).add(curr[0]);
-        }
-        for (int i = 0; i < numCourses; i++) {
-            if (!vis[i]) {
-                if (dfs(i, vis, pathVis, list))
-                    return false;
-            }
+        int n = numCourses;
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+        int degree[] = new int[n];
+        Queue<Integer> q = new ArrayDeque<>();
 
+        for(int i=0; i<n; i++){
+            arr.add(new ArrayList<>());
+        }
+        for(int curr[] : prerequisites){
+            arr.get(curr[0]).add(curr[1]);
+            degree[curr[1]] ++ ;
+        }
+        for(int i=0; i<n; i++){
+            if(degree[i] == 0) q.add(i);
+        }
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            // decrease the outgoing edges from this curr
+            for(int num : arr.get(curr)){
+                degree[num]--;
+                if(degree[num] == 0) q.add(num);
+            }
+        }
+        for(int num : degree){
+            if(num != 0) return false;
         }
         return true;
     }
-    public boolean dfs(int i, boolean[] vis, boolean[] pathVis, ArrayList<ArrayList<Integer>> list){
-        if(pathVis[i] == true) return true;
-        if(vis[i] == true) return false;
-        vis[i] = true;
-        pathVis[i] = true;
-        ArrayList<Integer> curr = list.get(i);
-        if(!curr.isEmpty()){
-            for(int bro : curr){
-                if(dfs(bro, vis, pathVis, list) == true) return true;
-
-            }
-        }
-        pathVis[i] = false;
-        return false;
-    }
-} 
+}
