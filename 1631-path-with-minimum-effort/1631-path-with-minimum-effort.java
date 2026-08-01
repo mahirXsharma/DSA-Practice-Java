9ba3fbcdab39b7a01be83f536d1fbc8a9ba48a0d
mahirXsharma <mahirsharma2002@gmail.com>
2026-08-01 15:33:35 +0530

    Time: 139 ms (5.04%), Space: 47.8 MB (75.15%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,37 +1,42 @@
 class Solution {
     public int minimumEffortPath(int[][] heights) {
-        int m = heights.length, n = heights[0].length;
-        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        PriorityQueue<int[]> pq = new PriorityQueue<>( (a,b)->a[0]-b[0] );
-        int dist[][] = new int[m][n];
-
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                dist[i][j] = Integer.MAX_VALUE;
+        int low = 0;
+        int high = 1000000;
+        int ans = -1;
+        // why equality though? ( silly question )
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(canReachEnd(heights, mid)){
+                ans = mid;
+                high = mid-1;
             }
+            else low = mid+1;
         }
-        dist[0][0] = 0;
-        pq.add(new int[]{0, 0, 0});
-
-        while( !pq.isEmpty()){
-            int[] curr = pq.poll();
-            int currEffort = curr[0];
-            int r = curr[1];
-            int c = curr[2];
-            if(r == m-1 && c == n-1) return currEffort;
-            for(int dir[] : dirs){
+        return ans;
+    }
+    private boolean canReachEnd(int heights[][], int mid){
+        int m= heights.length, n = heights[0].length;
+        int dirs[][] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        boolean vis[][] = new boolean[m][n];
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{0, 0});
+        vis[0][0] = true;
+        while( !q.isEmpty() ){
+            int curr[] = q.poll();
+            int r= curr[0], c = curr[1];
+            if(r == m-1 && c == n-1) return true;
+            for( int[] dir : dirs){
                 int nextR = r + dir[0];
                 int nextC = c + dir[1];
-                if(nextR>=0 && nextR < m && nextC >=0 && nextC < n){
+                if(nextR >= 0 && nextR < m && nextC < n && nextC >= 0){
                     int jump = Math.abs(heights[r][c] - heights[nextR][nextC]);
-                    int newEffort = Math.max(currEffort, jump);
-                    if(newEffort < dist[nextR][nextC]){
-                        dist[nextR][nextC] = newEffort;
-                        pq.add(new int[]{newEffort, nextR, nextC});
+                    if(!vis[nextR][nextC] && jump <= mid){
+                        vis[nextR][nextC] = true;
+                        q.add(new int[]{nextR, nextC});
                     }
                 }
             }
         }
-        return -1;
+        return false;
     }
 }
