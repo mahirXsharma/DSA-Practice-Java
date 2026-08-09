401261ec2d238556d120cd5455f95a2dfc0be37f
mahirXsharma <mahirsharma2002@gmail.com>
2026-08-09 12:54:54 +0530

    Time: 3 ms (5.07%), Space: 48.3 MB (32.91%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,25 +1,49 @@
 class Solution {
     public int trap(int[] height) {
-        // Calcualting the ans Vertically
+        // Calculating the ans Horizontally
         int n = height.length;
-        int left[] = new int[n];
-        int right[] = new int[n];
-        left[0] = height[0];
-        for(int i=1; i<n; i++){
-            left[i] = Math.max(left[i-1], height[i]);
-        }
-        right[n-1] = height[n-1];
-        for(int i=n-2; i>=0; i--){
-            right[i] = Math.max(right[i+1], height[i]);
-        }
-
+        Deque<Integer> s = new ArrayDeque<>();
         int ans = 0;
-        for(int i=0; i<n; i++){
-            int lb = left[i];
-            int rb = right[i];
-            int Height = Math.min(lb, rb) - height[i];
-            ans += Height;
+        for(int i=0; i<height.length; i++){
+            while(!s.isEmpty() && height[i] > height[s.peek()]){
+                // found the right wall
+                int bottom = height[s.pop()];
+                if(s.isEmpty()){
+                    break;
+                }
+                int leftWall = height[s.peek()];
+                int h = Math.min(leftWall, height[i]) - bottom;
+                int width = i - s.peek() - 1;
+                int currArea = h * width;
+                ans += currArea;
+            }
+            
+            s.push(i);
         }
         return ans;
+
+
+
+        // // Calcualting the ans Vertically
+        // int n = height.length;
+        // int left[] = new int[n];
+        // int right[] = new int[n];
+        // left[0] = height[0];
+        // for(int i=1; i<n; i++){
+        //     left[i] = Math.max(left[i-1], height[i]);
+        // }
+        // right[n-1] = height[n-1];
+        // for(int i=n-2; i>=0; i--){
+        //     right[i] = Math.max(right[i+1], height[i]);
+        // }
+
+        // int ans = 0;
+        // for(int i=0; i<n; i++){
+        //     int lb = left[i];
+        //     int rb = right[i];
+        //     int Height = Math.min(lb, rb) - height[i];
+        //     ans += Height;
+        // }
+        // return ans;
     }
 }
