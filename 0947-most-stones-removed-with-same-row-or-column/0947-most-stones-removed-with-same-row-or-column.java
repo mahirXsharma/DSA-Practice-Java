a51b53cb497b835520ad4e677bd1fc3cd2f553e5
mahirXsharma <mahirsharma2002@gmail.com>
2026-08-12 20:57:41 +0530

    Time: 31 ms (44.4%), Space: 46.3 MB (95.41%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,55 @@
+class Disjoint{
+    int[] parent;
+    int[] size;
+    public Disjoint(int n){
+        parent = new int[n];
+        size = new int[n];
+        for(int i=0; i<n; i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    public int findPar(int u){
+        if ( u == parent[u]) return u;
+        else{
+            parent[u] = findPar(parent[u]);
+            return parent[u];
+        }
+    }
+    public boolean find(int u, int v){
+        return findPar(u) == findPar(v);
+    }
+    
+    public void union(int u, int v){
+        int paru = findPar(u);
+        int parv = findPar(v);
+        if(paru == parv) return ;
+        if(size[paru] < size[parv]){
+            parent[paru] = parv;
+            size[parv] += size[paru];
+        }
+        else{
+            parent[parv] = paru;
+            size[paru] += size[parv];
+        }
+    }
+}
+
+class Solution {
+    public int removeStones(int[][] stones) {
+        int n = stones.length;
+        Disjoint ds = new Disjoint(n);
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]){
+                    ds.union(i, j);
+                }
+            }
+        }
+        int iso = 0;
+        for(int i=0; i<n; i++){
+            if(i == ds.parent[i]) iso++;
+        }
+        return n -iso;
+    }
+}
