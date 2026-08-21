54b70cd7dd9798ae1936c0b55b0bd5b9ca0d5f29
mahirXsharma <mahirsharma2002@gmail.com>
2026-08-21 14:49:32 +0530

    Time: 30 ms (74.03%), Space: 95.7 MB (27.58%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -6,14 +6,12 @@ class Solution {
             set.add(num);
         }
         int ans = 0;
-        for(int i=0; i<n; i++){
-            int curr = nums[i];
+        for(int curr : set){
 
             if(!set.contains(curr-1)){
                 int currNum = curr;
                 int streak = 1;
                 while(set.contains(currNum + 1)){
-                    set.remove(currNum+1);
                     currNum ++;
                     streak++;
                 }
