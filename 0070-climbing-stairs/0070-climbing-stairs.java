d54875b8a2587edaa7a5357e82ff106c2d5cae54
mahirXsharma <mahirsharma2002@gmail.com>
2026-08-24 22:56:11 +0530

    Time: 0 ms (100%), Space: 42 MB (54.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,25 @@
 class Solution {
     public int climbStairs(int n) {
-        int curr = 0;
-        int dp[] = new int[n+1];
-        return rec(n, curr, dp);
-    }
-    public int rec(int n, int curr, int dp[]){
-        if(curr == n) return 1;
-        if(curr > n) return 0;
-        if(dp[curr] != 0) return dp[curr];
-
-
-        int ways = rec(n, curr+1, dp) + rec(n, curr+2, dp);
-        return dp[curr] = ways;
-
-        
+         if(n == 1 || n == 2) return n;
+        int prev1 = 1, prev2 = 2;
+        int ans = 0;
+        for(int i=3; i<=n; i++){
+            ans = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = ans;
+        }
+        return ans;
     }
 
+
+    // TOP-DOWN
+    // public int rec(int n, int curr, int dp[]){
+    //     if(curr == n) return 1;
+    //     if(curr > n) return 0;
+    //     if(dp[curr] != 0) return dp[curr];
+
+
+    //     int ways = rec(n, curr+1, dp) + rec(n, curr+2, dp);
+    //     return dp[curr] = ways;
+    
 }
