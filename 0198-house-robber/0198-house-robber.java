82d9273cfbb3292a212c543d638351600bd33448
mahirXsharma <mahirsharma2002@gmail.com>
2026-08-26 15:14:51 +0530

    Time: 0 ms (100%), Space: 42.9 MB (19.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,9 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
         // 2 starting points, 0 or 1
-        int dp[] = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return dfs(nums, 0, dp);
+        int n = nums.length;
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        int dp[] = new int[n];
+        // Arrays.fill(dp, -1);
+        // return dfs(nums, 0, dp);
+
+        // TABULATION
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for(int i=2; i<n; i++){
+            int loot = nums[i] + dp[i-2];
+            int skip = dp[i-1];
+            dp[i] = Math.max(loot, skip);
+        }
+        return dp[n-1];
     }
     public int dfs(int nums[], int i, int dp[]){
         int n = nums.length;
