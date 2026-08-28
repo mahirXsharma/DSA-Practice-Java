84607c16064e2749593ba5f9fdd3a9823db6ee98
mahirXsharma <mahirsharma2002@gmail.com>
2026-08-28 14:42:11 +0530

    Time: 1 ms (17.23%), Space: 43.5 MB (45.75%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -3,7 +3,7 @@ class Solution {
         int m = obstacleGrid.length, n = obstacleGrid[0].length;
         if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
         int dp[][] = new int[m][n];
-        for(int row[] : dp) Arrays.fill(row, -1);
+        // for(int row[] : dp) Arrays.fill(row, -1);
         // TABULATION   
         dp[m-1][n-1] = 1;
         for(int i=m-1; i>=0; i--){
