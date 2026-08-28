42e2df117408808a1b59516166fa5bbdd6096c26
mahirXsharma <mahirsharma2002@gmail.com>
2026-08-28 14:46:14 +0530

    Time: 0 ms (100%), Space: 43.7 MB (26.83%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -5,22 +5,40 @@ class Solution {
         int dp[][] = new int[m][n];
         // for(int row[] : dp) Arrays.fill(row, -1);
         // TABULATION   
-        dp[m-1][n-1] = 1;
-        for(int i=m-1; i>=0; i--){
-            for(int j=n-1; j>=0; j--){
-                if(i == m-1 && j == n-1) continue;
+        dp[0][0] = 1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i == 0 && j == 0) continue;
                 if(obstacleGrid[i][j] == 1){
                     dp[i][j] = 0;
                     continue;
-                    }
-                // check the right and bottom
-                int right = 0, down = 0;
-                if(j+1 < n) right = dp[i][j+1];
-                if(i+1 < m) down = dp[i+1][j];
-                dp[i][j] = right + down;
+                }
+                int up = 0, left = 0;
+                if(i-1 >=0) up = dp[i-1][j];
+                if(j-1 >=0) left = dp[i][j-1];
+                dp[i][j] = up + left;
             }
         }
-        return dp[0][0];
+        return dp[m-1][n-1];
+
+
+
+        // dp[m-1][n-1] = 1;
+        // for(int i=m-1; i>=0; i--){
+        //     for(int j=n-1; j>=0; j--){
+        //         if(i == m-1 && j == n-1) continue;
+        //         if(obstacleGrid[i][j] == 1){
+        //             dp[i][j] = 0;
+        //             continue;
+        //             }
+        //         // check the right and bottom
+        //         int right = 0, down = 0;
+        //         if(j+1 < n) right = dp[i][j+1];
+        //         if(i+1 < m) down = dp[i+1][j];
+        //         dp[i][j] = right + down;
+        //     }
+        // }
+        // return dp[0][0];
         // return dfs(obstacleGrid, 0, 0, dp, m, n);
     }
     public int dfs(int obstacleGrid[][], int i, int j, int dp[][], int m, int n){
