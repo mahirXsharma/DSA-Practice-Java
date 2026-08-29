0c0a8b094301eb3412d6f3abad0091066a751014
mahirXsharma <mahirsharma2002@gmail.com>
2026-08-29 14:55:07 +0530

    Time: 3 ms (79.12%), Space: 45.6 MB (40.35%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,21 +1,21 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int m = triangle.size();
-        int dp[][] = new int[m][m];
+        int dp[] = new int[m];
         // for(int row[] : dp) Arrays.fill(row, (int)1e9);
         // return dfs(triangle, dp, 0, 0);
         // TABULATION
         List<Integer> last = triangle.get(m-1);
         for(int i=0; i<last.size(); i++){
-            dp[m-1][i] = last.get(i);
+            dp[i] = last.get(i);
         }
         for(int i=m-2; i>=0; i--){
             List<Integer> curr = triangle.get(i);
             for(int j=0; j<curr.size(); j++){
-                dp[i][j] = curr.get(j) + Math.min(dp[i+1][j], dp[i+1][j+1]);
+                dp[j] = curr.get(j) + Math.min(dp[j], dp[j+1]);
             }
         }
-        return dp[0][0];
+        return dp[0];
 
     }
     public int dfs( List<List<Integer>> list, int dp[][], int i, int idx){
