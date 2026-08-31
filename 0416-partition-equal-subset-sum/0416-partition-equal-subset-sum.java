f9fb78e95d39cc321efdd520ed8a5468d00c6585
mahirXsharma <mahirsharma2002@gmail.com>
2026-08-31 20:22:29 +0530

    Time: 49 ms (53.11%), Space: 47.6 MB (56.14%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -5,9 +5,28 @@ class Solution {
         for(int num : nums) ts+= num;
         if (ts % 2 != 0) return false;
         int target = ts/2;
-        Boolean dp[][] = new Boolean[target+1][n];
+        boolean[][] dp = new boolean[n][target + 1];
+        // TABULATION
+        // first fill the firt col, as its possible for every row to have a sum of 0
+        for(int i=0; i<n; i++)dp[i][0] = true;
+        if(nums[0] <= target) dp[0][nums[0]] = true;
 
-        return dfs(nums, target, 0, 0, dp);
+        for(int i=1; i<n; i++){
+            for(int j=1; j<=target; j++){
+                // in the case of leave, just copy from above
+                boolean leave = dp[i-1][j];
+                // lets assume by 'take', we didn't reached our ans
+                boolean take = false;
+                // check if target-nums[j] exist in the arr
+                if(nums[i] <= j){
+                    take = dp[i-1][j-nums[i]];
+                }
+                dp[i][j] = take || leave;
+            }
+        }
+        return dp[n-1][target];
+
+        // return dfs(nums, target, 0, 0, dp);
     }
     public boolean dfs(int nums[], int target, int curr, int idx, Boolean dp[][]){
         if(curr == target) return true;
