49c596d2de2128daa39214d98460043dd6ca09ec
mahirXsharma <mahirsharma2002@gmail.com>
2026-09-01 18:22:03 +0530

    Time: 3 ms (56.84%), Space: 42.4 MB (97.93%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -3,28 +3,23 @@ class Solution {
         int ts = 0, n = stones.length;
         for(int stone : stones) ts +=stone;
         int target = ts/2;
-        boolean dp[][] = new boolean[n][target+1];
+        boolean dp[] = new boolean[target+1];
         // filling the base case of col 1
-        for(int i=0; i<n; i++){
-            dp[i][0] = true;
-        }
+            dp[0] = true;
         // filling the base case for row zero
         if(stones[0] <= target){
-            dp[0][stones[0]]= true;
+            dp[stones[0]]= true;
         }
         for(int i=1; i<n; i++){
-            for(int j=1; j<=target; j++){
-                boolean leave = dp[i-1][j];
-                boolean take = false;
-                if(stones[i]<= j){
-                    take = dp[i-1][j-stones[i]];
-                }
-                dp[i][j] = leave || take;
+            for(int j=target; j>=stones[i]; j--){
+                boolean leave = dp[j];
+                boolean take = dp[j - stones[i]];
+                dp[j] = leave || take;
             }
         }
         int maxSumOfPileA = 0;
         for(int i=target; i>=0; i--){
-            if(dp[n-1][i]) {
+            if(dp[i]) {
                 maxSumOfPileA = i;
                 break;
             }
