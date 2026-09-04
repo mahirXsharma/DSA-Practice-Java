93f8a003d1bbc46b3d8b28835add804776207132
mahirXsharma <mahirsharma2002@gmail.com>
2026-09-04 15:36:17 +0530

    Time: 2 ms (99.95%), Space: 42.7 MB (97.36%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -10,43 +10,62 @@ class Solution {
         // 2. The Algebra Trick: Find the exact size of Pile A
         int tar = (ts + target) / 2;
 
-        // 3. Create the 2D DP Array
-        int[][] dp = new int[n][tar + 1];
 
-        // --- DAY 0: BASE CASES ---
-        // Trap Check: If the first number is 0, there are 2 ways to make sum 0 (Take or Leave)
-        if (nums[0] == 0) {
-            dp[0][0] = 2;
-        } else {
-            dp[0][0] = 1; // Otherwise, just 1 way to make sum 0 (Leave)
-        }
+        // 3. The 1D DP Array (Defaults to all 0s)
+        int[] dp = new int[tar + 1];
+        
+        // Base Case: 1 way to make a sum of 0 (pick nothing)
+        dp[0] = 1;
 
-        // If the first number isn't 0 and actually fits in our target, there is 1 way to make it (Take)
-        if (nums[0] != 0 && nums[0] <= tar) {
-            dp[0][nums[0]] = 1;
-        }
-
-        // --- DAY 1 ONWARDS: THE NESTED LOOPS ---
-        for (int i = 1; i < n; i++) {
-            // Notice j starts at 0 to catch those zero-sums!
-            for (int j = 0; j <= tar; j++) {
-
-                // Option 1: Leave the current number
-                int leave = dp[i - 1][j];
-
-                // Option 2: Take the current number (only if it fits inside j)
-                int take = 0;
-                if (nums[i] <= j) {
-                    take = dp[i - 1][j - nums[i]];
-                }
-
-                // Add the timelines together
-                dp[i][j] = leave + take;
+        // 4. The Loops (Outer: Items, Inner: Target backwards)
+        for (int num : nums) {
+            for (int j = tar; j >= num; j--) {
+                // dp[j] (leave) + dp[j - num] (take)
+                dp[j] = dp[j] + dp[j - num]; 
             }
         }
 
-        // 4. Return the bottom-right corner of our relevant grid
-        return dp[n - 1][tar];
+        return dp[tar];
+
+
+
+        // // 3. Create the 2D DP Array
+        // int[][] dp = new int[n][tar + 1];
+
+        // // --- DAY 0: BASE CASES ---
+        // // Trap Check: If the first number is 0, there are 2 ways to make sum 0 (Take or Leave)
+        // if (nums[0] == 0) {
+        //     dp[0][0] = 2;
+        // } else {
+        //     dp[0][0] = 1; // Otherwise, just 1 way to make sum 0 (Leave)
+        // }
+
+        // // If the first number isn't 0 and actually fits in our target, there is 1 way to make it (Take)
+        // if (nums[0] != 0 && nums[0] <= tar) {
+        //     dp[0][nums[0]] = 1;
+        // }
+
+        // // --- DAY 1 ONWARDS: THE NESTED LOOPS ---
+        // for (int i = 1; i < n; i++) {
+        //     // Notice j starts at 0 to catch those zero-sums!
+        //     for (int j = 0; j <= tar; j++) {
+
+        //         // Option 1: Leave the current number
+        //         int leave = dp[i - 1][j];
+
+        //         // Option 2: Take the current number (only if it fits inside j)
+        //         int take = 0;
+        //         if (nums[i] <= j) {
+        //             take = dp[i - 1][j - nums[i]];
+        //         }
+
+        //         // Add the timelines together
+        //         dp[i][j] = leave + take;
+        //     }
+        // }
+
+        // // 4. Return the bottom-right corner of our relevant grid
+        // return dp[n - 1][tar];
     }
 
     // public int dfs(int nums[], int tar, int idx, int cs, int dp[][]){
