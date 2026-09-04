b683557ff7beb42804ea592271ef08b73fdb6200
mahirXsharma <mahirsharma2002@gmail.com>
2026-09-04 14:36:52 +0530

    Time: 9 ms (54.95%), Space: 47.1 MB (14.18%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -2,16 +2,21 @@ class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         
         int n = nums.length;
-        return dfs(nums, target, 0, 0);
+        int ts = 0;
+        for(int num : nums) ts += num;
+        int dp[][] = new int[n][2*ts + 1];
+        for(int row[] : dp) Arrays.fill(row, -1);
+        return dfs(nums, target, 0, 0, dp, ts);
     }
 
-    public int dfs(int nums[], int tar, int cs, int idx){
+    public int dfs(int nums[], int tar, int cs, int idx, int dp[][], int ts){
         if(idx == nums.length){
             return cs == tar? 1 : 0;
         }
-        int takeminus = dfs(nums, tar, cs -nums[idx], idx + 1);
-        int takeplus = dfs(nums, tar, cs + nums[idx], idx + 1);
+        if(dp[idx][cs+ts] != -1) return dp[idx][cs + ts];
+        int takeminus = dfs(nums, tar, cs -nums[idx], idx + 1, dp, ts);
+        int takeplus = dfs(nums, tar, cs + nums[idx], idx + 1, dp, ts);
         int ans = takeminus + takeplus;
-        return ans ;
+        return dp[idx][cs + ts] = ans ;
     }
 }
