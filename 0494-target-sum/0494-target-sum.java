64d50a054ef27aaf55aa509478966eb15485009f
mahirXsharma <mahirsharma2002@gmail.com>
2026-09-04 14:58:33 +0530

    Time: 498 ms (29.28%), Space: 43.1 MB (79.14%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,22 +1,16 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        
-        int n = nums.length;
         int ts = 0;
-        for(int num : nums) ts += num;
-        int dp[][] = new int[n][2*ts + 1];
-        for(int row[] : dp) Arrays.fill(row, -1);
-        return dfs(nums, target, 0, 0, dp, ts);
+        for(int num : nums) ts +=  num;
+        if((ts+target) %2 != 0 || Math.abs(target) > ts) return 0;
+        int tar = (ts + target)/ 2;
+        return dfs(nums, tar, 0, 0);
     }
 
-    public int dfs(int nums[], int tar, int cs, int idx, int dp[][], int ts){
-        if(idx == nums.length){
-            return cs == tar? 1 : 0;
-        }
-        if(dp[idx][cs+ts] != -1) return dp[idx][cs + ts];
-        int takeminus = dfs(nums, tar, cs -nums[idx], idx + 1, dp, ts);
-        int takeplus = dfs(nums, tar, cs + nums[idx], idx + 1, dp, ts);
-        int ans = takeminus + takeplus;
-        return dp[idx][cs + ts] = ans ;
+    public int dfs(int nums[], int tar, int idx, int cs){
+        if(idx == nums.length) return cs == tar ? 1 : 0;
+        int leave = dfs(nums, tar, idx+1, cs );
+        int take = dfs(nums, tar, idx+1, cs + nums[idx]);
+        return leave + take;
     }
 }
