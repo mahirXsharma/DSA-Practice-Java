50986f1460b642cb01ed2f5190b473a756983eba
MAHIR SHARMA <mahirsharma2002@gmail.com>
2026-09-11 20:46:18 +0530

    Time: 13 ms (85.63%), Space: 43 MB (81.73%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,26 +1,29 @@
 class Solution {
     public int numDistinct(String s, String t) {
         int n1 = t.length(), n2 = s.length();
-        int dp[][] = new int[n1+1][n2+1];
+        int dp[] = new int[n1+1];
         // for (int row[] : dp)
         //     Arrays.fill(row, -1);
         // return dfs(0, 0, s, t, dp);
 
         // TABULATION
-        for(int j=0; j<=n2; j++){
-            dp[n1][j] = 1;
-        }
-        for(int i=n1-1; i>=0; i--){
-            for(int j=n2-1; j>=0; j--){
-                int take = 0;
-                if(s.charAt(j) == t.charAt(i)){
-                    take = dp[i+1][j+1];
-                }
-                int leave = dp[i][j+1];
-                dp[i][j] = take + leave;
+        // for(int j=0; j<=n2; j++){
+        //     dp[n1][j] = 1;
+        // }
+        dp[n1] = 1; 
+
+    // Outer loop scans the source string 's' backwards
+    for (int j = n2 - 1; j >= 0; j--) {
+        // Inner loop scans the target string 't' FORWARDS
+        for (int i = 0; i < n1; i++) {
+            if (s.charAt(j) == t.charAt(i)) {
+                // take + leave
+                dp[i] = dp[i + 1] + dp[i]; 
             }
+            // If they don't match, dp[i] remains just dp[i] (the "leave" state)
         }
-        return dp[0][0];
+    }
+    return dp[0];
     }
 
     public int dfs(int i, int idx, String s, String t, int dp[][]){
