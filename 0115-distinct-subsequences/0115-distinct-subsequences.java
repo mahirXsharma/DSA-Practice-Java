02bbf20c237e3a52d1031db42a0c0794f632d618
MAHIR SHARMA <mahirsharma2002@gmail.com>
2026-09-11 16:26:16 +0530

    Time: 20 ms (44.99%), Space: 54.5 MB (23.99%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,9 +1,26 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int dp[][] = new int[t.length()][s.length()];
-        for (int row[] : dp)
-            Arrays.fill(row, -1);
-        return dfs(0, 0, s, t, dp);
+        int n1 = t.length(), n2 = s.length();
+        int dp[][] = new int[n1+1][n2+1];
+        // for (int row[] : dp)
+        //     Arrays.fill(row, -1);
+        // return dfs(0, 0, s, t, dp);
+
+        // TABULATION
+        for(int j=0; j<=n2; j++){
+            dp[n1][j] = 1;
+        }
+        for(int i=n1-1; i>=0; i--){
+            for(int j=n2-1; j>=0; j--){
+                int take = 0;
+                if(s.charAt(j) == t.charAt(i)){
+                    take = dp[i+1][j+1];
+                }
+                int leave = dp[i][j+1];
+                dp[i][j] = take + leave;
+            }
+        }
+        return dp[0][0];
     }
 
     public int dfs(int i, int idx, String s, String t, int dp[][]){
