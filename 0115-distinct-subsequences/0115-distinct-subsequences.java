04f970575a73c9f4ade8e3b919a89842f086d116
MAHIR SHARMA <mahirsharma2002@gmail.com>
2026-09-11 15:59:47 +0530

    Time: 20 ms (44.99%), Space: 54.5 MB (34.03%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,25 +1,22 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        StringBuilder sb = new StringBuilder();
         int dp[][] = new int[t.length()][s.length()];
         for (int row[] : dp)
             Arrays.fill(row, -1);
-        return dfs(0, 0, s, t, sb, dp);
+        return dfs(0, 0, s, t, dp);
     }
 
-    public int dfs(int i, int idx, String s, String t, StringBuilder sb, int dp[][]){
-        if(i == t.length() || idx == s.length()){
-            if(sb.toString().equals(t)) return 1;
-            else return 0;
-        }
+    public int dfs(int i, int idx, String s, String t, int dp[][]){
+            if(i == t.length()) return 1;
+            if(idx == s.length()) return 0;
+
         if(dp[i][idx] != -1) return dp[i][idx];
         int take = 0;
         if(s.charAt(idx) == t.charAt(i)) { 
-            take = dfs(i+1, idx+1, s, t, sb.append(s.charAt(idx)), dp);
-            sb.deleteCharAt(sb.length()-1);
+            take = dfs(i+1, idx+1, s, t, dp);
         }
         
-        int leave = dfs(i, idx+1, s, t, sb, dp);
+        int leave = dfs(i, idx+1, s, t, dp);
         return dp[i][idx] = take + leave;
     }
 }
