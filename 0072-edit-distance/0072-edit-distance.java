4e404886ea148c5a3d1e9b84a48b521610e420b8
MAHIR SHARMA <mahirsharma2002@gmail.com>
2026-09-15 17:12:05 +0530

    Time: 5 ms (65.87%), Space: 47.2 MB (35.15%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,9 +1,28 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         int n1 = word1.length(), n2 = word2.length();
-        int dp[][] = new int[n1][n2];
-        for(int row[] : dp) Arrays.fill(row, -1);
-     return dfs(word1, word2, n1-1, n2-1, dp);   
+        int dp[][] = new int[n1+1][n2+1];
+        //     for(int row[] : dp) Arrays.fill(row, -1);
+        //  return dfs(word1, word2, n1-1, n2-1, dp);   
+
+        // TABULATION
+        for(int i=0; i<=n1; i++) dp[i][0] = i;
+        for(int j=0; j<=n2; j++) dp[0][j] = j;
+
+        for(int i=1; i<=n1; i++){
+            for(int j=1; j<=n2; j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    int up = dp[i-1][j];
+                    int left = dp[i][j-1];
+                    int diag = dp[i-1][j-1];
+                    dp[i][j] = 1 + Math.min(left, Math.min(diag, up));
+                }
+            }
+        }
+        return dp[n1][n2];
     }
     public int dfs(String s1, String s2, int i, int j, int dp[][]){
         if(i < 0){
