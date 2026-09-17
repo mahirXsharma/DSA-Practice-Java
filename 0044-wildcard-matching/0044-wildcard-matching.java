40c10b3bf66099e84ac03e9b8975ccf6522f1f75
MAHIR SHARMA <mahirsharma2002@gmail.com>
2026-09-17 13:10:55 +0530

    Time: 95 ms (6.42%), Space: 62.8 MB (23.57%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        Boolean[][] dp = new Boolean[s.length() + 1][p.length() + 1];
+        int[][] dp = new int[s.length() + 1][p.length() + 1];
         return dfs(s, p, 0, 0, dp);
     }
-
-    private boolean dfs(String s, String p, int i, int j, Boolean[][] dp) {
-
+    private boolean dfs(String s, String p, int i, int j, int[][] dp) {
         if (j == p.length()) {
             return i == s.length();
         }
-
 
         if (i == s.length()) {
             for (int k = j; k < p.length(); k++) {
@@ -19,9 +16,8 @@ class Solution {
             }
             return true;
         }
-
-        if (dp[i][j] != null) {
-            return dp[i][j];
+        if (dp[i][j] != 0) {
+            return dp[i][j] == 1 ? true : false;
         }
 
         boolean match = false;
@@ -34,7 +30,7 @@ class Solution {
         } else {
             match = false;
         }
-
-        return dp[i][j] = match;
+        dp[i][j] = match ==true ? 1 : -1;
+        return match;
     }
 }
