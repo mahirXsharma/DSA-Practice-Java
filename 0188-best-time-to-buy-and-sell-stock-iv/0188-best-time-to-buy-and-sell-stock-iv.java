949db69e3d7259c7e34a8beb24808153d7ce77f8
MAHIR SHARMA <mahirsharma2002@gmail.com>
2026-09-19 12:47:17 +0530

    Time: 2 ms (71.06%), Space: 43.3 MB (83.34%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,31 +1,42 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[2][k+1][n+1];
+        // int[][][] dp = new int[2][k+1][n+1];
         // for(int matrix[][] : dp){
         //     for(int row[] : matrix){
         //         Arrays.fill(row, -1);
         //     }
         // }
         // return dfs(prices, 0, 0, 0, k, n, dp);
+        int front[][] = new int[2][k+1];
+        int curr[][] = new int[2][k+1];
+
         for(int idx = n-1; idx>=0; idx--){
             for(int tc = 0; tc<k; tc++){
-                for(int isH = 0; isH<=1; isH++){
+                for (int isH = 0; isH <= 1; isH++) {
+                    
                     int skip = 0;
                     int trans = 0;
-                    if(isH == 0){
-                        skip = dp[0][tc][idx + 1];
-                        trans = -prices[idx] + dp[1][tc][idx+1];
+                    
+                    if (isH == 0) { 
+                        skip = front[0][tc];
+
+                        trans = -prices[idx] + front[1][tc]; 
+                    } else { 
+
+                        skip = front[1][tc];
+
+                        trans = prices[idx] + front[0][tc + 1]; 
                     }
-                    else{
-                        skip = dp[1][tc][idx+1];
-                        trans = prices[idx] + dp[0][tc+1][idx+1];
-                    }
-                    dp[isH][tc][idx] = Math.max(skip, trans);
+                    curr[isH][tc] = Math.max(skip, trans);
                 }
             }
+            for(int t = 0; t <= k; t++){
+                front[0][t] = curr[0][t];
+                front[1][t] = curr[1][t];
+            }
         }
-        return dp[0][0][0];
+        return front[0][0];
     }
     
     public int dfs(int prices[], int isHolding, int tc, int idx, int k, int n, int[][][] dp){
