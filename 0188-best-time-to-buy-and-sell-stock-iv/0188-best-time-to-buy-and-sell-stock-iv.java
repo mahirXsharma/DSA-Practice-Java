671a6c5301749bc7d24311a4424a0bbe8eb608cc
MAHIR SHARMA <mahirsharma2002@gmail.com>
2026-09-19 12:28:40 +0530

    Time: 3 ms (49.47%), Space: 45.1 MB (57%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,13 +1,31 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[3][k][n];
-        for(int matrix[][] : dp){
-            for(int row[] : matrix){
-                Arrays.fill(row, -1);
+        int[][][] dp = new int[2][k+1][n+1];
+        // for(int matrix[][] : dp){
+        //     for(int row[] : matrix){
+        //         Arrays.fill(row, -1);
+        //     }
+        // }
+        // return dfs(prices, 0, 0, 0, k, n, dp);
+        for(int idx = n-1; idx>=0; idx--){
+            for(int tc = 0; tc<k; tc++){
+                for(int isH = 0; isH<=1; isH++){
+                    int skip = 0;
+                    int trans = 0;
+                    if(isH == 0){
+                        skip = dp[0][tc][idx + 1];
+                        trans = -prices[idx] + dp[1][tc][idx+1];
+                    }
+                    else{
+                        skip = dp[1][tc][idx+1];
+                        trans = prices[idx] + dp[0][tc+1][idx+1];
+                    }
+                    dp[isH][tc][idx] = Math.max(skip, trans);
+                }
             }
         }
-        return dfs(prices, 0, 0, 0, k, n, dp);
+        return dp[0][0][0];
     }
     
     public int dfs(int prices[], int isHolding, int tc, int idx, int k, int n, int[][][] dp){
