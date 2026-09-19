ecaf8d5a101aa1e5a711501beb463b9894ff678b
MAHIR SHARMA <mahirsharma2002@gmail.com>
2026-09-19 11:28:02 +0530

    Time: 49 ms (45.37%), Space: 159.2 MB (5.45%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,36 +1,22 @@
 class Solution {
     public int maxProfit(int[] prices) {
-
-        Integer[][][] dp = new Integer[prices.length][3][2];
-        
-        return dfs(prices, 0, 0, 0, dp);
+        Integer[][][] dp = new Integer[2][3][prices.length];
+        return dfs(prices, 0, 0, 0, prices.length, dp);
     }
 
-    private int dfs(int[] prices, int idx, int tc, int isHolding, Integer[][][] dp) {
-        if (idx == prices.length || tc == 2) {
-            return 0; 
-        }
-
-        if (dp[idx][tc][isHolding] != null) {
-            return dp[idx][tc][isHolding];
-        }
-
+    public int dfs(int prices[], int isHolding, int tc, int idx, int n, Integer dp[][][]){
+        if(tc == 2 || idx == n) return 0;
+        if(dp[isHolding][tc][idx] != null) return dp[isHolding][tc][idx];
         int skip = 0;
-        int transact = 0;
-
-        if (isHolding == 1) { 
-            
-            skip = dfs(prices, idx + 1, tc, 1, dp);
- 
-            transact = prices[idx] + dfs(prices, idx + 1, tc + 1, 0, dp);
-            
-        } else { 
-
-            skip = dfs(prices, idx + 1, tc, 0, dp);
-            
-            transact = -prices[idx] + dfs(prices, idx + 1, tc, 1, dp);
+        int trans = 0;
+        if(isHolding == 1){
+            skip = dfs(prices, 1, tc, idx+1, n, dp);
+            trans = prices[idx] + dfs(prices, 0, tc + 1, idx + 1, n, dp);
         }
-
-        return dp[idx][tc][isHolding] = Math.max(skip, transact);
+        else{
+            skip = dfs(prices, 0, tc, idx+1, n, dp);
+            trans = -prices[idx] + dfs(prices, 1, tc, idx+1, n, dp);
+        }
+        return  dp[isHolding][tc][idx] = Math.max(skip, trans);
     }
 }
