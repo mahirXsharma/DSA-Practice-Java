c80fd8fc03b079fe107566e6238f7ab4269f4838
MAHIR SHARMA <mahirsharma2002@gmail.com>
2026-09-21 20:26:33 +0530

    Time: 31 ms (29.49%), Space: 74.3 MB (33.21%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int dp[][] = new int[n][3];
+        for(int row[] : dp) Arrays.fill(row, -1);
+        return dfs(prices, fee, 0, 0, dp);
+    }
+
+    public int dfs(int prices[], int fee, int idx, int isHolding, int dp[][]){
+        if(idx == prices.length) return 0;
+        if(dp[idx][isHolding] != -1) return dp[idx][isHolding];
+        int ans = 0;
+        if(isHolding == 0){
+            // we are not holding a stock right now
+            int buy = -prices[idx] + dfs(prices, fee, idx+1, 1, dp);
+            int leave = dfs(prices, fee, idx+1, 0, dp);
+            ans = Math.max(buy, leave);
+        }
+        else{
+            int sell = prices[idx] - fee + dfs(prices, fee, idx+1, 0, dp);
+            int hold = dfs(prices, fee, idx+1, 1, dp);
+            ans = Math.max(ans, Math.max(sell, hold));
+        }
+        return dp[idx][isHolding] = ans;
+    }
+}
