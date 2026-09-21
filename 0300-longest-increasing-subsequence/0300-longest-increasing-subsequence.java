1a33c3ecc4cfc6613c92d4c5ef8c3f0e1858b6ac
MAHIR SHARMA <mahirsharma2002@gmail.com>
2026-09-21 23:23:06 +0530

    Time: 448 ms (5.02%), Space: 235.9 MB (24.23%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,31 +1,23 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int ans = 0, n = nums.length;
-        if(n ==1) return 1;
-
-        int dp[][] = new int[n][n];
+        int n = nums.length;
+        int dp[][] = new int[n][n+1];
         for(int row[] : dp) Arrays.fill(row, -1);
 
-        for(int i=0; i<n; i++){
-            int curr = dfs(nums, i, i, dp);
-            ans = Math.max(ans, curr);
-        }
-        return ans;
+        return dfs(nums, 0, -1, dp, n);
     }
 
-    public int dfs(int nums[], int idx, int prevIdx, int dp[][]){
-        if(idx == nums.length) return 1; 
-        if(dp[idx][prevIdx] != -1) return dp[idx][prevIdx];
-        int val = nums[idx];
-        int ans = 0;
-        if(val >  nums[prevIdx]){
-            // take or leave
-            int take = dfs(nums, idx+1, idx, dp) +1;
-            int leave = dfs(nums, idx+1, prevIdx, dp);
-            ans = Math.max(take, leave);
+    public int dfs(int nums[], int idx, int prevIdx, int dp[][], int n){
+        if(idx == n) return 0;
+        if(dp[idx][prevIdx+1] != -1) return dp[idx][prevIdx+1];
+
+        int leave = dfs(nums, idx+1, prevIdx, dp, n);
+        int take = 0;
+        if(prevIdx == -1 || nums[prevIdx] < nums[idx]){
+            take = dfs(nums, idx+1, idx, dp, n) + 1;
         }
-        ans = Math.max(ans, dfs(nums, idx+1, prevIdx, dp));
+        return  dp[idx][prevIdx+1] = Math.max(leave, take);
         
-        return dp[idx][prevIdx] = ans;
     }
+
 }
