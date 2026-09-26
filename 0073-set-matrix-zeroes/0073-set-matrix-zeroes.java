211c216ed3ce4d0197f8ac86a042e987d1b7675a
MAHIR SHARMA <mahirsharma2002@gmail.com>
2026-09-26 19:09:38 +0530

    Time: 1 ms (95.95%), Space: 47.7 MB (24.07%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,18 +3,15 @@ class Solution {
         int col0 = 1;
         int m = matrix.length, n = matrix[0].length;
 
-        for(int j=0; j<m; j++){
-            if(col0 == 0) break;
-            if(matrix[j][0] == 0) col0 = 0;
-        }
-
         for(int i=0; i<m; i++){
+            if(matrix[i][0]== 0) col0 = 0;
             for(int j=1; j<n; j++){
                 if(matrix[i][j] == 0){
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
                 }
             }
+            
         }
 
         for(int i=m-1; i>=0; i--){
@@ -23,11 +20,8 @@ class Solution {
                     matrix[i][j] = 0;
                 } 
             }
-        }
-
-        if(col0 == 0){
-            for(int j=0; j<m; j++){
-                matrix[j][0] = 0;
+            if(col0 == 0){
+                matrix[i][0] = 0;
             }
         }
     }
