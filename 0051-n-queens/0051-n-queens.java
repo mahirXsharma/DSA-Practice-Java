13dbcf9d343ad6daa933ac629402777ed5e9e8dc
MAHIR SHARMA <mahirsharma2002@gmail.com>
2026-09-26 23:00:31 +0530

    Time: 1 ms (99.84%), Space: 46.4 MB (90.99%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -4,12 +4,14 @@ class Solution {
         for(char row[] : matrix) Arrays.fill(row, '.');
 
         List<List<String>> ans = new ArrayList<>();
-
-        dfs(matrix, 0, ans, n);
+        boolean isCol[] = new boolean[n];
+        boolean left[] = new boolean[2*n-1];
+        boolean right[] = new boolean[2*n-1];
+        dfs(matrix, 0, ans, n, isCol, left, right);
         return ans;
     }
 
-    public void dfs(char matrix[][], int row, List<List<String>> ans, int n){
+    public void dfs(char matrix[][], int row, List<List<String>> ans, int n, boolean isCol[], boolean left[], boolean right[]){
         if(row == n){
             List<String> validPath = new ArrayList<>();
             for(int i=0; i<n; i++){
@@ -21,35 +23,23 @@ class Solution {
 
         for(int i=0; i<n; i++){
             // is this safe ? if yes then  place the Queen
+            int minor = row+i;
+            int major = row-i + n-1;
 
-            if(isSafe(row, i, matrix, n)){
+            if(!isCol[i] && !left[major] && !right[minor]){
                 matrix[row][i] = 'Q';
-                dfs(matrix, row+1, ans, n);
+                isCol[i]=true;
+                left[major] = true;
+                right[minor] = true;
+
+                dfs(matrix, row+1, ans, n, isCol, left, right);
                 matrix[row][i] = '.';
+
+                isCol[i]=false;
+                left[major] = false;
+                right[minor] = false;
             }
         }
     }
-    public boolean isSafe(int row, int col, char matrix[][], int n){
-        // when it is safe?? -> top, left & right diag !
-        for(int i = row-1; i >= 0; i--){
-            if(matrix[i][col] == 'Q') return false;
-        }
-
-        int currRow = row-1, currCol = col-1;
-        while(currRow >= 0 && currCol >= 0){
-            if(matrix[currRow][currCol] == 'Q') return false;
-            currRow--;
-            currCol--;
-        }
-
-        currRow = row-1;
-        currCol = col+1;
-        while(currRow >= 0 && currCol < n){
-            if(matrix[currRow][currCol] == 'Q') return false;
-            currRow--;
-            currCol++;
-        }
-        return true;
-
-    }
+    
 }
