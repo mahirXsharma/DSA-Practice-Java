d00c68b91dbf612bdc723f50600caebb6e7fb9d8
MAHIR SHARMA <mahirsharma2002@gmail.com>
2026-09-26 19:58:03 +0530

    Time: 81 ms (26.97%), Space: 157.9 MB (21.63%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.java
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.java
@@ -1,19 +1,50 @@
 class Solution {
     public List<Integer> findLonely(int[] nums) {
+
         List<Integer> ans = new ArrayList<>();
-        Arrays.sort(nums);
-        int n =nums.length;
-        
-        for(int i=0; i<n; i++){
-            boolean lonely = true;
-            if(i+1 < n ){
-                if(nums[i+1] == nums[i] + 1 || nums[i+1] == nums[i]) lonely = false;
-            }
-            if(i-1 >= 0){
-                if( nums[i-1] == nums[i] || nums[i-1] == nums[i]-1)lonely = false;
-            } 
-            if(lonely) ans.add(nums[i]);
+        // HASHMAP
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        return ans;        
+
+        for(int num : nums){
+            boolean lonely = true;
+            if(map.get(num)>1) lonely = false;
+            if(map.containsKey(num-1) || map.containsKey(num+1)) lonely = false;
+            if(lonely) ans.add(num);
+        }
+        return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Arrays.sort(nums);
+        // int n =nums.length;
+        // for(int i=0; i<n; i++){
+        //     boolean lonely = true;
+        //     if(i+1 < n ){
+        //         if(nums[i+1] == nums[i] + 1 || nums[i+1] == nums[i]) lonely = false;
+        //     }
+        //     if(i-1 >= 0){
+        //         if( nums[i-1] == nums[i] || nums[i-1] == nums[i]-1)lonely = false;
+        //     } 
+        //     if(lonely) ans.add(nums[i]);
+        // }
+        // return ans;        
     }
 }
